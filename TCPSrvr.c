ab1fd2f182124eb6fcd95dc7143727af60da4c38
suvani-ctrl <sssubbbi1234@gmail.com>
2024-11-17 15:03:22 +0545

    Update TCPSrvr.c

--- TCPSrvr.c
+++ TCPSrvr.c
@@ -1,59 +1,65 @@
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <arpa/inet.h>
+#include <strings.h> // For bzero
+#include <string.h>  // For strlen
 
 int main() {
     int server_sockFD, client_sockFD;
     struct sockaddr_in serverAddress, clientAddress;
-    int ClientAddressLength;
+    socklen_t clientAddressLength;
 
     server_sockFD = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sockFD < 0) {
-        perror("Socket not created");
+    if (server_sockFD == -1) {
+        perror("There was an error connecting to the socket");
         exit(1);
     } else {
         printf("Socket: %d\n", server_sockFD);
     }
 
+    // BINDING THE SOCKET -----> STEP 2 <-------------
     bzero(&serverAddress, sizeof(serverAddress));
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddress.sin_port = htons(8080);
 
-    if (bind(server_sockFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
-        perror("Binding failed");
-        close(server_sockFD);
-        exit(1);
-    }
-
-    if (listen(server_sockFD, 1) != 0) {
-        perror("Listen failed");
-        close(server_sockFD);
+    if (bind(server_sockFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) != 0) {
+        printf("Failed to Bind !\n");
         exit(1);
     } else {
-        printf("The server is listening :-)\n");
+        printf("Bind was successful!\n");
+    }
+
+    // LISTENING TO THE SOCKET --------------> STEP 3 <--------------
+    if (listen(server_sockFD, 1) != 0) {
+        printf("Failed to listen!\n");
+        exit(1);
+    } else {
+        printf("The server is listening for incoming connections\n");
     }
 
     bzero(&clientAddress, sizeof(clientAddress));
-    ClientAddressLength = sizeof(clientAddress);
-
-    client_sockFD = accept(server_sockFD, (struct sockaddr*)&clientAddress, &ClientAddressLength);
+    clientAddressLength = sizeof(clientAddress);
+    client_sockFD = accept(server_sockFD, (struct sockaddr *)&clientAddress, &clientAddressLength);
     if (client_sockFD == -1) {
-        printf("Failed to accept!\n");
+        printf("Failed to accept\n");
         exit(1);
     } else {
         printf("Accepted!\n");
     }
 
-    char msg[] = "Hi Suvani <3";
-    write(client_sockFD, msg, strlen(msg));
-    printf("Sent: %s\n", msg);
+    // Sending a message to the client
+    char *msg = "Hello Suvani";
+    if (write(client_sockFD, msg, strlen(msg)) == -1) {
+        perror("Error sending message to the client");
+    } else {
+        printf("Message sent: %s\n", msg);
+    }
 
     close(client_sockFD);
     close(server_sockFD);
 
     return 0;
 }
-                                   
